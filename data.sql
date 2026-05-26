5a0a943970127c97bebc6337a3b19eb6adf38820
Nilesh Palve <nilesh.palve@kinesso.com>
2026-05-26 15:11:02 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-insert into employee(100,'Nilesh',85000);
-insert into employee(200,'Nikhil',55000);
-insert into employee(300,'Mahesh',70000);
-insert into employee(400,'Nimish',60000);
+insert into employee values(100,'Nilesh',85000);
+insert into employee values(200,'Nikhil',55000);
+insert into employee values(300,'Mahesh',70000);
+insert into employee values(400,'Nimish',60000);
