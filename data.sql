f7af489b83c8a29d25d3d13a2a60a5d4e08e57ad
NileshPalve-KIN <snowflake_aws@rediffmail.com>
2026-05-26 13:45:38 +0000

    added values in insert statement

--- data.sql
+++ data.sql
@@ -1,3 +1,3 @@
-insert into employee(100,'Nilesh',85000);
-insert into employee(200,'Nikhil',85000);
-insert into employee(300,'Mahesh',85000);
+insert into employee values(100,'Nilesh',85000);
+insert into employee values(200,'Nikhil',85000);
+insert into employee values(300,'Mahesh',85000);
